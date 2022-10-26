0ad0d844ac9ccb82e2cf0be19fee750f5764c7fa
mvcardim <68335567+mvcardim@users.noreply.github.com>
2022-10-26 11:29:45 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -3,7 +3,9 @@
 echo "Atualizando o servidor..."
 dnf update
 dnf upgrade -y
-dnf install apache2 -y
+dnf install httpd -y
+systemctl start httpd.service
+chkconfig --levels 235 httpd on
 dnf install unzip -y
 
 
@@ -13,6 +15,7 @@ cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
+sudo su
 cp -R * /var/www/html/
 
 
